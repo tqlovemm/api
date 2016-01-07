4a5867d580259da44fb312a39ad2fce76ccb66ca
tqmm <397114202@qq.com>
2016-01-07 12:36:10 +0800

    分玩法发

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=182.254.217.147;dbname=shisan',
+            'username' => 'root',
+            'password' => '@shisan2015@',
+            'charset' => 'utf8',
+            'tablePrefix' => 'pre_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
