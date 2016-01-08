e063a129da93e0863eeb036adacf5735050c3398
tqmm <397114202@qq.com>
2016-01-08 13:33:09 +0800

    发文

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=shisan',
             'username' => 'root',
-            'password' => '',
+            'password' => 'qzwx@ecrv',
             'charset' => 'utf8',
             'tablePrefix' => 'pre_',
         ],
