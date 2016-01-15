d84d49b245c3f12642be08cb4f33225809f27943
tqmm <397114202@qq.com>
2016-01-15 17:16:27 +0800

    fewatgafwe

--- api/modules/v2/controllers/NoteController.php
+++ api/modules/v2/controllers/NoteController.php
@@ -6,14 +6,25 @@ use Yii;
 use yii\helpers\Response;
 use yii\web\Controller;
 
-
 class NoteController extends Controller
 {
 
    public function actionView($id){
 
-       Yii::$app->db->createCommand("UPDATE {{%forum_thread}} SET note=note+1 WHERE id=".$id)->execute();
-       Response::show(202,'点赞成功');
+       $user_id = $_GET['user_id'];
+
+       $query = Yii::$app->db->createCommand("select * from pre_user_notes WHERE thread_id=".$id." and note_by=".$user_id)->execute();
+
+       if(empty($query)){
+
+           Yii::$app->db->createCommand("UPDATE {{%forum_thread}} SET note=note+1 WHERE id=".$id)->execute();
+           Yii::$app->db->createCommand("insert into {{%user_notes}} (note_by,note_at,thread_id) values ({$user_id},".time().",{$id})")->execute();
+           Response::show(202,'点赞成功');
+
+       }else{
+
+           Response::show(402,'已经存在');
+       }
 
    }
 
