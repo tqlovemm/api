28e69a8be863d6719948ab619030268e4e541ddd
tqmm <397114202@qq.com>
2016-01-18 13:21:58 +0800

    fewatgafwe

--- api/modules/v2/controllers/NoteController.php
+++ api/modules/v2/controllers/NoteController.php
@@ -11,19 +11,27 @@ class NoteController extends Controller
 
    public function actionView($id){
 
+
+
        $user_id = $_GET['user_id'];
 
-       $query = Yii::$app->db->createCommand("select * from pre_user_notes WHERE thread_id=".$id." and note_by=".$user_id)->execute();
+       $query = Yii::$app->db->createCommand("select * from {{%user_notes}} WHERE thread_id=".$id." and note_by=".$user_id)->execute();
+
 
        if(empty($query)){
 
            Yii::$app->db->createCommand("UPDATE {{%forum_thread}} SET note=note+1 WHERE id=".$id)->execute();
            Yii::$app->db->createCommand("insert into {{%user_notes}} (note_by,note_at,thread_id) values ({$user_id},".time().",{$id})")->execute();
-           Response::show(202,'点赞成功');
+           $query_one = Yii::$app->db->createCommand("select * from {{%forum_thread}} WHERE id=".$id)->queryOne();
+
+           Response::show(202,'点赞成功',$query_one['note']);
 
        }else{
 
-           Response::show(402,'已经存在');
+           $query_one = Yii::$app->db->createCommand("select * from {{%forum_thread}} WHERE id=".$id)->queryOne();
+
+           Response::show(402,'已经存在',$query_one['note']);
+
        }
 
    }
