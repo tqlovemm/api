acde5e94eb767ba1146f49ad1aba83e33e857bb6
tqmm <397114202@qq.com>
2016-01-27 11:06:11 +0800

    fewatgafwe

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=shisan',
             'username' => 'root',
-            'password' => '',
+            'password' => '@shisan2015@',
             'charset' => 'utf8mb4',
             'tablePrefix' => 'pre_',
         ],
