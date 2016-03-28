e1e803b6a683363839fb19009381e213f57ac924
tqmm <397114202@qq.com>
2016-03-28 14:32:04 +0800

    fewatgafwe

---
+++ api/modules/v2/controllers/FlopContentDataController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace api\modules\v2\controllers;
+
+
+use Yii;
+use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yii\helpers\Response;
+
+
+class FlopContentDataController extends ActiveController
+{
+    public $modelClass = 'api\modules\v2\models\FlopContentData';
+
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        // token 验证  请按需开启
+        /* $behaviors['authenticator'] = [
+             'class' => CompositeAuth::className(),
+             'authMethods' => [
+                 QueryParamAuth::className(),
+             ],
+         ];*/
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        // 注销系统自带的实现方法
+        unset($actions['index'], $actions['update'], $actions['create'], $actions['delete'], $actions['view']);
+        return $actions;
+    }
+
+    public function actionIndex()
+    {
+        if(isset($_GET['user_id'])){
+
+            $user_id = $_GET['user_id'];
+
+        }else{
+
+            return false;
+        }
+        $modelClass = $this->modelClass;
+
+        $query = $modelClass::find()->where(['user_id'=>$user_id])->orderBy('created_at desc');
+
+        return new ActiveDataProvider([
+            'query' => $query,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+
+        $model = new $this->modelClass();
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+        if (!$model->save()) {
+
+            return array_values($model->getFirstErrors())[0];
+        }
+
+        Response::show('202','保存成功');
+
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+
+
+
+        if (!$model->save()) {
+
+            return array_values($model->getFirstErrors())[0];
+
+        }
+
+        return $model;
+
+    }
+
+    public function actionDelete($id)
+    {
+        if($this->findModel($id)->delete()){
+            Response::show('202','删除成功');
+        }
+    }
+
+    public function actionView($id)
+    {
+        $model = $this->findModel($id);
+        return $model;
+    }
+
+    protected function findModel($id)
+    {
+        $modelClass = $this->modelClass;
+        if (($model = $modelClass::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+}
---
+++ api/modules/v2/models/FlopContentData.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace api\modules\v2\models;
+
+use app\components\db\ActiveRecord;
+use Yii;
+
+
+/**
+ * This is the model class for table "pre_flop_content_data".
+ *
+ * @property integer $id
+ * @property string $content
+ * @property string $priority
+ * @property integer $user_id
+ * @property integer $flag
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class FlopContentData extends ActiveRecord
+{
+
+    public static function tableName()
+    {
+        return '{{%flop_content_data}}';
+    }
+
+    public function rules()
+    {
+        return [
+
+            [['user_id','content','priority'],'required'],
+            [['created_at', 'updated_at','user_id'], 'integer'],
+            [['content', 'priority'], 'string'],
+        ];
+    }
+
+
+    public function fields()
+    {
+        $fields = parent::fields();
+        $fields["flop_content_data_id"] = $fields['id'];
+        // remove fields that contain sensitive information
+
+        return $fields;
+
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'content' => '内容',
+            'priority' => '翻牌',
+            'user_id' => '用户ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+}
