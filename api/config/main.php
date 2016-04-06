a38e38cf7accfb085122a0b973d458594b3f21b1
tqmm <397114202@qq.com>
2016-04-06 16:59:46 +0800

    fewatgafwe

--- api/config/main.php
+++ api/config/main.php
@@ -52,7 +52,7 @@ return [
                 [
                     'class' => 'yii\rest\UrlRule',
                     'controller' => [
-                      'v3/dating','v3/dating-content','v3/dating-comment',
+                      'v3/dating','v3/dating-content','v3/dating-comment','v3/date','v3/version',
                     ],
                     'tokens' => [
                         '{id}' => '<id:\\w+>',
