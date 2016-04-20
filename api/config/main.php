10e84b8817bcc437678dd5d99a2ccb1a91e2ae69
tqmm <397114202@qq.com>
2016-04-20 10:57:08 +0800

    fewatgafwe

--- api/config/main.php
+++ api/config/main.php
@@ -52,7 +52,7 @@ return [
                 [
                     'class' => 'yii\rest\UrlRule',
                     'controller' => [
-                      'v3/dating','v3/dating-content','v3/dating-comment','v3/date','v3/version','v3/push',
+                      'v3/dating','v3/dating-content','v3/dating-comment','v3/date','v3/version','v3/push','v3/heartweek','v3/slide-content',
                     ],
                     'tokens' => [
                         '{id}' => '<id:\\w+>',
