e1c55f3f50e24e6fd85f65419b6ffebc39e304d1
tqmm <397114202@qq.com>
2016-07-21 14:13:26 +0800

    fewatgafwe

--- api/modules/v2/models/FlopContent.php
+++ api/modules/v2/models/FlopContent.php
@@ -57,7 +57,7 @@ class FlopContent extends ActiveRecord
             'flop_id','like_count','nope_count','weight','height',
             'area','number','content'=>function($model){
 
-                return json_encode($model['content']);
+                return serialize($model['content']);
 
             }, 'path'
 
